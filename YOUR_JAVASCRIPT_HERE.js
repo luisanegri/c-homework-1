3d58915914930863f24cf1cb7b24f135f88e432f
luisanegri <luisaanegri@gmail.com>
2019-10-12 15:18:46 +0200

    function that allows user to click on image and fills up health status, health status displayed on page, alert if status health is full

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -2,9 +2,26 @@ let hero = {
   name: 'Lu',
   heroic: true,
   inventory: [],
-  health: 2,
+  health: 10,
   weapon: {
     type: 'riffle',
     damage: 2
   }
 };
+
+function rest(person) {
+  document.getElementById('health-stats').innerHTML = person.health;
+  document.getElementById('inn').addEventListener('click', function() {
+    if (person.health === 10) {
+      alert('You are 100% healthy!');
+    } else {
+      person.health = 10;
+      //  Option2: Object.assign(person, { health: 10 });
+    }
+    document.getElementById('health-stats').innerHTML = person.health;
+  });
+
+  return person;
+}
+
+rest(hero);
